a6d855d36770ca4464b4b76ad6221903db0155fa
pietjepuck <machieldeboer@hotmail.nl>
2017-06-28 12:08:41 +0200

    Update minion.sh
    minion update werkt

--- minion.sh
+++ minion.sh
@@ -1,5 +1,19 @@
-sudo apt-get install salt-minion
-cd /etc/salt
+#sudo apt update && sudo apt-get upgrade
 
-sudo cp - f minion.d minion.d
+function move() {
+    # code here
+    cd /etc
+}
+move
+sudo echo "172.0.1.1 machiel-minoin" | sudo tee hosts
+sudo apt-get install salt-minion -y
+
+function move2() {
+    # code here
+    cd ~/home/ubuntu
+}
+
+sudo git clone https://github.com/pietjepuck/saltstack.git 
+sudo cp -f saltstack/minion /etc/salt
+sudo salt-minion -d
 
