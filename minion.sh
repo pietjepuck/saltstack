0b0dd486fb7cdad703207da1fa7f49117cd288ec
pietjepuck <machieldeboer@hotmail.nl>
2017-06-29 18:18:49 +0200

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -1,19 +1,24 @@
 #sudo apt update && sudo apt-get upgrade
 
+
 function move() {
     # code here
     cd /etc
 }
 move
-sudo echo "172.0.1.1 machiel-minoin" | sudo tee hosts
+#https://blackboard.hanze.nl/webapps/assessment/do/authoring/modifyAssessment?blackboard.platform.security.NonceUtil.nonce=14117a6e-4519-4276-bcef-3ebed4cbcbee&method=modifyAssessment&copyAlignments=false&course_id=_338564_1&assessmentId=_637848_1&sectionId=&questionId=&saveAsNew=false&createAnother=false&assessmentType=Pool&isLinkedQuestion=&referencingQuestionId=
+HST="$(hostname)" 
+sudo sed -i "/127.0.0.1/ a\127.0.0.1 $HST" /etc/hosts
 sudo apt-get install salt-minion -y
 
 function move2() {
     # code here
-    cd ~/home/ubuntu
+    cd ~/
 }
+move2
 
-sudo git clone https://github.com/pietjepuck/saltstack.git 
+sudo git clone https://github.com/pietjepuck/saltstack.git
+sudo chmod +x ~/saltstack/install.sh  
 sudo cp -f saltstack/minion /etc/salt
 sudo salt-minion -d
 
