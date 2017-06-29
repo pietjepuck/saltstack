272fcdbf21e8de94f3fa7138e49b7d576a888492
pietjepuck <machieldeboer@hotmail.nl>
2017-06-29 18:23:31 +0200

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -9,6 +9,7 @@ move
 #https://blackboard.hanze.nl/webapps/assessment/do/authoring/modifyAssessment?blackboard.platform.security.NonceUtil.nonce=14117a6e-4519-4276-bcef-3ebed4cbcbee&method=modifyAssessment&copyAlignments=false&course_id=_338564_1&assessmentId=_637848_1&sectionId=&questionId=&saveAsNew=false&createAnother=false&assessmentType=Pool&isLinkedQuestion=&referencingQuestionId=
 HST="$(hostname)" 
 sudo sed -i "/127.0.0.1/ a\127.0.0.1 $HST" /etc/hosts
+
 sudo apt-get install salt-minion -y
 
 function move2() {
