80a2f5379113c0dd975c05d26985d3c897fbf4d7
pietjepuck <machieldeboer@hotmail.nl>
2017-06-29 10:30:28 +0200

    Create install.sh
    eerste comit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install docker.io -y
+sudo docker pull mariadb
+sudo mkdir /opt/wordpress
+sudo mkdir -p /opt/wordpress/database
+sudo mkdir -p /opt/wordpress/html
+sudo mkdir /var/www
+sudo mkdir -p /var/www/html
+
+sudo docker run -e MYSQL_ROOT_PASSWORD=mysqllalala -e MYSQL_USER=wpuser -e MYSQL_PASSWORD=mysqllalala -e MYSQL_DATABASE=wordpress_db -v /opt/wordpress/database:/var/lib/mysql --name wordpressdb -d mariadb
+
+sudo docker inspect -f '{{ .NetworkSettings.IPAddress }}' wordpressdb
+
+sudo docker pull wordpress:latest
+
+sudo docker run -e WORDPRESS_DB_USER=wpuser -e WORDPRESS_DB_PASSWORD=mysqllalala -e WORDPRESS_DB_NAME=wordpress_db -p 8081:80 -v /opt/wordpress/html:/var/www/html --link wordpressdb:mysql --name wpcontainer -d wordpress
+
+#http://www.techrepublic.com/article/how-to-install-wordpress-with-docker/
